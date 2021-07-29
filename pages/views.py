bf2ec1cbde6b29018660670d3f7b359bccc0e6d4
fathan <fathan.pranaya@gmail.com>
2021-07-29 13:07:46 +0900

    added admin asset

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,13 @@
+from django.http import JsonResponse
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'pages/home.html'
+    def get(self, request, *args, **kwargs):
+        # return JsonResponse({'status': 'oke'})
+        return render(request, 'pages/home.html')
+    # template_name = 'pages/home.html'
 
 
 class AboutPageView(TemplateView):
