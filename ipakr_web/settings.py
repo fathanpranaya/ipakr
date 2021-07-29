a8a70f468d1f117fce8d7dca2591a712e8ad5d9b
fathan <fathan.pranaya@gmail.com>
2021-07-29 11:31:44 +0900

    updated mysql

--- ipakr_web/settings.py
+++ ipakr_web/settings.py
@@ -81,10 +81,21 @@ TEMPLATES = [
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ipakr_web',
+        'USER': 'root',
+        'PASSWORD': 'idea0999!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
